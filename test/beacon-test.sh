0ed0fee234778161acb3bbb4d8aec5cc3b40e7a9
gilgil <gilgil@gilgil.net>
2020-11-01 20:38:16 +0900

    Update beacon-test.sh

--- test/beacon-test.sh
+++ test/beacon-test.sh
@@ -1,9 +1,15 @@
-if [ $# -ne 2 ]; then
-	echo "syntax : beacon-test.sh <interface> <ap mac>"
-	echo "sample : beacon-test.sh mon0 90:9F:33:D9:A0:E0"
+if [ $# -lt 2 ]; then
+	echo "syntax : beacon-test.sh <interface> <ap mac> [<bitmap>]"
+	echo "sample : beacon-test.sh mon0 00:00:00:11:11:11"
 	exit 1
 fi
 INTERFACE="$1"
 AP_MAC="$2"
 
-sudo tshark -i $INTERFACE -T fields -e frame.time_relative -e frame.time_delta_displayed -e radiotap.length -e wlan.seq -e wlan.fixed.timestamp -e wlan.tim.partial_virtual_bitmap -Y "wlan.addr==$AP_MAC && wlan.fc.type_subtype==8"
+FILTER="wlan.addr==$AP_MAC && wlan.fc.type_subtype==8"
+if [ $# -eq 3 ]; then
+	FILTER+=" && wlan.tim.bmapctl==$3"
+fi
+
+echo "FILTER="$FILTER
+sudo tshark -i $INTERFACE -T fields -e frame.time_relative -e frame.time_delta_displayed -e radiotap.length -e wlan.seq -e wlan.fixed.timestamp -e wlan.tim.partial_virtual_bitmap -Y "$FILTER"
