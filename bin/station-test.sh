10ceffada3d81327866a51c010774e01931bbefa
gilgil <gilgil@gilgil.net>
2020-11-16 16:17:17 +0900

    Change sample mac address

--- bin/station-test.sh
+++ bin/station-test.sh
@@ -1,6 +1,6 @@
 if [ $# -ne 2 ]; then
 	echo "syntax : station-test.sh <interface> <ap mac>"
-	echo "sample : station-test.sh mon0 E4:F8:9C:67:E4:CC"
+	echo "sample : station-test.sh mon0 00:00:00:11:11:11"
 	exit 1
 fi
 INTERFACE="$1"
